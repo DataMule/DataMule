4c664995a04a14c7dbffc76fd68738b8ca2e6ae9
mrpoor <greg15001@gmail.com>
2018-03-24 14:20:40 -0400

    fixed up datamule

--- datamule/core/data_mule.py
+++ datamule/core/data_mule.py
@@ -17,23 +17,24 @@ class DataMule():
     def run(self):
         self._get_db()
         for connector in self.connectors:
-            self.connection_string = connector['connector']['connection_string']
-            self.schema = connector['connector']['schema']
-            self.table_name = connector['connector']['table_name']
-            self.delta_type = connector['connector']['delta']['type']
-            self.delta_value = connector['connector']['delta']['value']
-            self._upload_to_db()
-            self._insert_data_process()
+            connection_string = connector['connector']['connection_string']
+            schema = connector['connector']['schema']
+            table_name = connector['connector']['table_name']
+            delta_type = connector['connector']['delta']['type']
+            delta_value = connector['connector']['delta']['value']
+            self._upload_to_db(connection_string)
+            self._insert_data_process(table_name, delta_value)
 
 
 
     def _get_schema(self):
         if (self.schema == 'auto'):
-            parser = Parser(self.format)
-            if self.format == 'csv':
-                return parser.create_schema(self.connector)
-            elif self.format == 'json':
-                return parser.create_schema(self.connector)
+            for connector in self.connectors:
+                parser = Parser(self.format_type)
+                if self.format_type == 'csv':
+                    return parser.create_schema(connector['connector']['connection_string'])
+                elif self.format_type == 'json':
+                    return parser.create_schema(connector['connector']['connection_string'])
 
     def _get_db(self):
         data_mule_docker = DataMuleDocker(self.db_type)
@@ -41,14 +42,14 @@ class DataMule():
         self.user_name = dict_user_password['user_name']
         self.password = dict_user_password['password']
 
-    def _upload_to_db(self):
+    def _upload_to_db(self, connection_string):
         uploader = Uploader(self.db_type, self.user_name, self.password)
         if self.protocol == 'http':
-            uploader.upload_http(self.connection_string)
+            uploader.upload_http(connection_string)
 
-    def _insert_data_process(self):
+    def _insert_data_process(self, table_name, delta_value):
         data_process = DataProcess()
-        data_process.insert_process(name=self.table_name, local_or_container='container',
-                                    delta=self.delta_value, table_name=self.table_name, datasource_type=self.db_type)
+        data_process.insert_process(name=table_name, local_or_container='container',
+                                    delta=delta_value, table_name=table_name, datasource_type=self.db_type)
 
 
