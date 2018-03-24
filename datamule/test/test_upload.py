555b2a44525ede0c9803d90114132ed6d521af2d
Chris Glazner <c.glazner@celmatix.com>
2018-03-24 15:01:07 -0400

    Get the data

--- datamule/test/test_upload.py
+++ datamule/test/test_upload.py
@@ -1,8 +1,12 @@
 import datamule.core.upload
+import datamule.core.data_mule
 import os
 
 ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
-uploader = datamule.core.upload.Uploader("postgres")
+
+dm = datamule.core.data_mule.DataMule() # Busted
+uploader = datamule.core.upload.Uploader("postgres", dm.user_name, dm.password)
+
 
 params = {"url": "https://sandbox.api.it.nyu.edu/course-catalog-exp/courses",
           "headers": {'Authorization': 'Bearer {}'.format(ACCESS_TOKEN)}}
